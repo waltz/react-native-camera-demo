b97fe92f11c43211bdfbcf4b22ed47e94d452f78
CJ Bryan <christianbryan@gmail.com>
2017-03-01 15:36:48 -0800

    Upgrade to react-native 0.42.

--- android/app/src/main/java/com/mobile/MainApplication.java
+++ android/app/src/main/java/com/mobile/MainApplication.java
@@ -1,14 +1,15 @@
 package com.mobile;
 
 import android.app.Application;
-import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import com.lwansbrough.ReactCamera.ReactCamera;
-import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import com.lwansbrough.ReactCamera.ReactCamera;
+import com.facebook.react.ReactInstanceManager;
 
 import java.util.Arrays;
 import java.util.List;
@@ -17,7 +18,7 @@ public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
-    protected boolean getUseDeveloperSupport() {
+    public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
     }
 
@@ -32,6 +33,12 @@ public class MainApplication extends Application implements ReactApplication {
 
   @Override
   public ReactNativeHost getReactNativeHost() {
-      return mReactNativeHost;
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
   }
 }
