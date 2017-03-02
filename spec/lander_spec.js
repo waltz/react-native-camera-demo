faf76e74964f7037d4ef044c58871fd4ae9b91e4
CJ Bryan <christianbryan@gmail.com>
2017-03-01 17:46:34 -0800

    WIP: Use Mocha to test individual React components.

---
+++ spec/lander_spec.js
@@ -0,0 +1,11 @@
+import assert from 'assert';
+// import Lander from '../lander.js';
+
+describe('<Lander/>', () => {
+  describe('render()', () => {
+    it('renders', () => {
+      const lander = 1;
+      assert.equal(lander, 2);
+    });
+  });
+});
